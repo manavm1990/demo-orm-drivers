64ecd44a490a0af2ba5d8dda7e59c84bc29a9501
Manav Misra <manavm@visionwebsoft.com>
2023-01-11 17:30:30 -0600

    Add starter files

--- app/index.js
+++ app/index.js
@@ -1,1 +1,19 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import bookRouter from "./book/routes.js";
+
+const app = express();
+
+const PORT = 3001;
+
+// Middleware to parse incoming request bodies (e.g. POST - CREATE)
+app.use(express.json());
+
+app.use("/api/books", bookRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
