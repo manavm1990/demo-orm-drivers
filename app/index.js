5eb1f5c9ffb585881228cb38ee0e29c1f901fefc
Manav Misra <manavm@visionwebsoft.com>
2023-01-11 19:26:05 -0600

    Initialize 💩

--- app/index.js
+++ app/index.js
@@ -1,19 +1,5 @@
-import express from "express";
-import bookRouter from "./book/routes.js";
+import initDB from "./db-setup.js";
+import initServer from "./server.js";
 
-const app = express();
-
-const PORT = 3001;
-
-// Middleware to parse incoming request bodies (e.g. POST - CREATE)
-app.use(express.json());
-
-app.use("/api/books", bookRouter);
-
-app.use((_, res) => {
-  res.status(404).json({ message: "Not found" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+initDB();
+initServer();
---
+++ app/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const server = express();
+
+const PORT = 3001;
+
+export default function init() {
+  // Middleware to parse incoming request bodies (e.g. POST - CREATE)
+  server.use(express.json());
+
+  server.use((_, res) => {
+    res.status(404).json({ message: "Not found" });
+  });
+
+  server.listen(PORT, () => {
+    console.info(`Server listening on port ${PORT}`);
+  });
+}
